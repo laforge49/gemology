b6d288df3fb24de03dc2e7c9c3805d3f463ba04b
Bill La Forge <laforge49@gmail.com>
2024-11-29 19:06:40 -0500

    Update core.py

--- gems/core.py
+++ gems/core.py
@@ -3,54 +3,6 @@ import pathlib
 import copy
 
 
-def get_cluster_id_gem_base_name(gem: dict | None) -> str | None:
-    return get_cluster_id_name(gem, "gem_base_name")
-
-
-def make_facet_cluster_ids(gem: dict | None) -> dict | None:
-    if gem is None:
-        return None
-    cluster_ids_facet = get_facet_cluster_ids(gem)
-    if cluster_ids_facet is None:
-        cluster_ids_facet = {}
-        gem["ClusterIdsFacet"] = cluster_ids_facet
-    return cluster_ids_facet
-
-
-def get_facet_inverted_cluster_ids(cluster: dict | None) -> dict | None:
-    if cluster is None:
-        return None
-    return cluster.get("#InvertedClusterIdsFacet")
-
-
-def get_inverted_cluster_ids(cluster: dict | None, id_type: str) -> dict | None:
-    inverted_cluster_ids_facet = get_facet_inverted_cluster_ids(cluster)
-    if inverted_cluster_ids_facet is None:
-        return None
-    return inverted_cluster_ids_facet.get(id_type)
-
-
-def get_inverted_cluster_id(cluster: dict | None, id_type: str, id: str) -> dict | None:
-    inverted_cluster_ids = get_inverted_cluster_ids(cluster, id_type)
-    if inverted_cluster_ids is None:
-        return None
-    return inverted_cluster_ids.get(id)
-
-
-def get_gem_by_base_name(cluster: dict | None, gem_base_name: str) -> dict | None:
-    return get_inverted_cluster_id(cluster, "gem_base_name", gem_base_name)
-
-
-def make_facet_inverted_cluster_ids(cluster: dict | None) -> dict | None:
-    if cluster is None:
-        return None
-    inverted_cluster_ids_facet = get_facet_inverted_cluster_ids(cluster)
-    if inverted_cluster_ids_facet is None:
-        inverted_cluster_ids_facet = {}
-        cluster["#InvertedClusterIdsFacet"] = inverted_cluster_ids_facet
-    return inverted_cluster_ids_facet
-
-
 def get_facet_cluster_tags(gem: dict | None) -> dict | None:
     if gem is None:
         return None
