8ea2c064642ebdae5c5dbdfbac01b6cdc2d5d9b3
Bill La Forge <laforge49@gmail.com>
2024-11-30 17:30:29 -0500

    Update local_tags_facet.py

--- gems/facets/local_tags_facet.py
+++ gems/facets/local_tags_facet.py
@@ -46,21 +46,40 @@ def cluster_get_tag_names(gem: dict | None) -> base.dict_keys | None:
     return ltif.keys()
 
 
-def cluster_get_tag_values(gem: dict | None, id_type: str) -> base.dict_keys | None:
+def cluster_get_tag_values(gem: dict | None, tag_name: str) -> base.dict_keys | None:
     ltif = get_ltif(gem)
     if ltif is None:
         return None
-    ltif2 = ltif.get(id_type)
+    ltif2 = ltif.get(tag_name)
     if ltif2 is None:
         return None
     return ltif2.keys()
 
 
-def cluster_get_gems_by_id(gem: dict | None, id_type: str, id_name: str) -> list | None:
+def cluster_get_gems_by_tag(gem: dict | None, tag_name: str, tag_value: str) -> list | None:
     ltif = get_ltif(gem)
     if ltif is None:
         return None
-    ltif2 = ltif.get(id_type)
+    ltif2 = ltif.get(tag_name)
     if ltif2 is None:
         return None
-    return ltif2.get(id_name)
+    return ltif2.get(tag_value)
+
+
+def del_id(gem: dict | None, tag_name: str, tag_value: str) -> bool:
+    if gem is None:
+        return False
+    ltf = get_ltf(gem)
+    if ltf is None:
+        return False
+    values = ltf.get(tag_name)
+    if values is None:
+        return False
+    if tag_value not in values:
+        return False
+    values.remove(tag_value)
+    ltif = get_ltif(gem)
+    ltif2 = ltif.get(tag_name)
+    gems = ltif2.get(tag_value)
+    gems.remove(gem)
+    return True
