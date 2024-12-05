8030f3387d582646afd1907c0e600530b5477a18
Bill La Forge <laforge49@gmail.com>
2024-12-05 15:10:23 -0500

    forgot

--- gems/core.py
+++ gems/core.py
@@ -1,5 +1,6 @@
 import pathlib
 
+from gems import base
 from gems.facets import gems_query, local_ids_update, local_tags_update, attrs_update, global_ids_update, \
     global_tags_update, attrs_query
 from pdml import loader, saver
@@ -26,7 +27,7 @@ def register(cluster: dict, cluster_name: str, cluster_path: str) -> None:
         global_tags_update.build_index(gem)
 
 
-def load_cluster(cluster_path: pathlib.Path) -> dict:
+def load(cluster_path: pathlib.Path) -> dict:
     cluster_file_name = cluster_path.name
     cluster_name = cluster_file_name.split(".")[0]
     cluster = loader.file_reader(cluster_path)
@@ -34,12 +35,38 @@ def load_cluster(cluster_path: pathlib.Path) -> dict:
     return cluster
 
 
-def save_cluster(cluster: dict) -> None:
+def save(cluster: dict) -> None:
     cluster_path = attrs_query.get_attr_value(cluster, "#cluster_path")
     saver.writer(cluster_path, cluster)
 
 
-def unplug_cluster(cluster: dict) -> None:
+def save_as(cluster: dict, cluster_path) -> None:
+    saver.writer(cluster_path, cluster)
+
+
+def unplug(cluster: dict) -> None:
     for gem, _ in gems_query.get_gems(cluster, None):
         global_ids_update.deindex(gem)
         global_tags_update.deindex(gem)
+
+
+def test(home_path: pathlib.Path) -> None:
+    print()
+    print("*** core test ***")
+    test_data_path = home_path / pathlib.Path("test data")
+    print("test data path:", test_data_path)
+    sample1 = load(test_data_path / pathlib.Path("sample1.pdml"))
+    print()
+    print("aggregate:")
+    print(base.get_aggregate())
+    saver.debug(base.get_aggregate())
+    print()
+    print("sample1")
+    print(sample1)
+    saver.debug(sample1)
+    save_as(sample1, test_data_path / pathlib.Path("sample1a.pdml"))
+
+
+if __name__ == "__main__":
+    home_path = pathlib.Path.cwd()
+    test(home_path)
