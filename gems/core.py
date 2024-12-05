98dc16b339e6d85f2bdc35bd93782fc79b1a434a
Bill La Forge <laforge49@gmail.com>
2024-12-04 21:06:49 -0500

    Update core.py

--- gems/core.py
+++ gems/core.py
@@ -1,5 +1,16 @@
+import pathlib
+
 from gems.facets import gems_query, local_ids_update, local_tags_update, attrs_update, global_ids_update, \
-    global_tags_update
+    global_tags_update, attrs_query
+from pdml import loader, saver
+
+
+def create_gem(cluster: dict, gem_parent: dict, gem_base_name: str) -> dict | None:
+    gem = {}
+    attrs_update.set_cluster(gem, cluster)
+    attrs_update.set_gem_parent(gem, gem_parent)
+    local_ids_update.set_gem_base_name(gem, gem_base_name)
+    return gem
 
 
 def register(cluster: dict, cluster_name: str, cluster_path: str) -> None:
@@ -15,14 +26,20 @@ def register(cluster: dict, cluster_name: str, cluster_path: str) -> None:
         global_tags_update.build_index(gem)
 
 
-def unplug(cluster: dict) -> None:
+def load_cluster(cluster_path: pathlib.Path) -> dict:
+    cluster_file_name = cluster_path.name
+    cluster_name = cluster_file_name.split(".")[0]
+    cluster = loader.file_reader(cluster_path)
+    register(cluster, cluster_name, str(cluster_path))
+    return cluster
+
+
+def save_cluster(cluster: dict) -> None:
+    cluster_path = attrs_query.get_attr_value(cluster, "#cluster_path")
+    saver.writer(cluster_path, cluster)
+
+
+def unplug_cluster(cluster: dict) -> None:
     for gem, _ in gems_query.get_gems(cluster, None):
         global_ids_update.deindex(gem)
-
-
-def create_gem(cluster: dict, gem_parent: dict, gem_base_name: str) -> dict | None:
-    gem = {}
-    attrs_update.set_cluster(gem, cluster)
-    attrs_update.set_gem_parent(gem, gem_parent)
-    local_ids_update.set_gem_base_name(gem, gem_base_name)
-    return gem
+        global_tags_update.deindex(gem)
