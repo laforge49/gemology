5e31b4a50db11750739ad796942516cd5eaa510f
Bill La Forge <laforge49@gmail.com>
2024-12-11 10:56:13 -0500

    rooot window

--- tkgems/tkfacets/tkattrs.py
+++ tkgems/tkfacets/tkattrs.py
@@ -3,6 +3,22 @@ import typing
 from gems.facets import attrs_update, attrs_query
 
 
+def get_is_widget(tkdescriptor_gem: dict | None) -> bool | None:
+    return attrs_query.get_attr_value(tkdescriptor_gem, "is_widget")
+
+
+def set_is_widget(tkdescriptor_gem: dict | None, is_widget: bool) -> bool:
+    return attrs_update.set_attr_value(tkdescriptor_gem, "is_widget", is_widget)
+
+
+def get_packable(tkdescriptor_gem: dict | None) -> bool | None:
+    return attrs_query.get_attr_value(tkdescriptor_gem, "packable")
+
+
+def set_packable(tkdescriptor_gem: dict | None, packable: bool) -> bool:
+    return attrs_update.set_attr_value(tkdescriptor_gem, "packable", packable)
+
+
 def get_tkcomposer(tkdescriptor_gem: dict | None) -> typing.Callable | None:
     return attrs_query.get_attr_value(tkdescriptor_gem, "#tkcomposer")
 
@@ -11,17 +27,15 @@ def set_tkcomposer(tkdescriptor_gem: dict | None, tkcomposer: typing.Callable) -
     return attrs_update.set_attr_value(tkdescriptor_gem, "#tkcomposer", tkcomposer)
 
 
-def get_is_widget(tkdescriptor_gem: dict | None) -> bool:
-    return attrs_query.get_attr_value(tkdescriptor_gem, "is_widget")
+def get_tkheight(tkgem: dict | None) -> int | None:
+    h = attrs_query.get_attr_value(tkgem, "height")
+    if h:
+        return int(h)
+    return None
 
 
-def set_is_widget(tkdescriptor_gem: dict | None, is_widget: bool) -> bool:
-    return attrs_update.set_attr_value(tkdescriptor_gem, "is_widget", is_widget)
-
-
-def get_packable(tkdescriptor_gem: dict | None) -> bool:
-    return attrs_query.get_attr_value(tkdescriptor_gem, "packable")
-
-
-def set_packable(tkdescriptor_gem: dict | None, packable: bool) -> bool:
-    return attrs_update.set_attr_value(tkdescriptor_gem, "packable", packable)
+def get_tkwidth(tkgem: dict | None) -> int | None:
+    w = attrs_query.get_attr_value(tkgem, "width")
+    if w:
+        return int(w)
+    return None
