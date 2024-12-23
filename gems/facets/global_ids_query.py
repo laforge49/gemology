2c26ed444a9e0e4d2c5ac999b7a597e4dc3bff6f
Bill La Forge <laforge49@gmail.com>
2024-12-23 13:41:42 -0500

    Update global_ids_query.py

--- gems/facets/global_ids_query.py
+++ gems/facets/global_ids_query.py
@@ -1,40 +1,42 @@
+from typing import *
+
+
 from gems import base
-from gems.facets import attrs_query
 
 
-def get_gif(gem: dict | None) -> dict | None:
+def get_gif(gem: Optional[base.Gem]) -> Optional[dict]:
     if gem is None:
         return None
     return gem.get("GlobalIdsFacet")
 
 
-def gem_get_id_types(gem: dict | None) -> base.dict_keys | None:
+def gem_get_id_types(gem: Optional[base.Gem]) -> Optional[base.dict_keys]:
     gif = get_gif(gem)
     if gif is None:
         return None
     return gif.keys()
 
 
-def gem_get_id_name(gem: dict | None, id_type: str) -> str | None:
+def gem_get_id_name(gem: Optional[base.Gem], id_type: str) -> Optional[str]:
     gif = get_gif(gem)
     if gif is None:
         return None
     return gif.get(id_type)
 
 
-def get_giif() -> dict | None:
+def get_giif() -> Optional[dict]:
     aggregate = base.get_aggregate()
     return aggregate.get("#GlobalIdIndexFacet")
 
 
-def aggregate_get_id_types() -> base.dict_keys | None:
+def aggregate_get_id_types() -> Optional[base.dict_keys]:
     giif = get_giif()
     if giif is None:
         return None
     return giif.keys()
 
 
-def aggregate_get_id_names(id_type: str) -> base.dict_keys | None:
+def aggregate_get_id_names(id_type: str) -> Optional[base.dict_keys]:
     giif = get_giif()
     if giif is None:
         return None
@@ -44,7 +46,7 @@ def aggregate_get_id_names(id_type: str) -> base.dict_keys | None:
     return giif2.keys()
 
 
-def aggregate_get_gem_by_id(id_type: str, id_name: str) -> dict | None:
+def aggregate_get_gem_by_id(id_type: str, id_name: str) -> Optional[dict]:
     giif = get_giif()
     if giif is None:
         return None
@@ -54,13 +56,13 @@ def aggregate_get_gem_by_id(id_type: str, id_name: str) -> dict | None:
     return giif2.get(id_name)
 
 
-def get_cluster_name(cluster: dict | None) -> str | None:
+def get_cluster_name(cluster: Optional[base.Cluster]) -> Optional[str]:
     return gem_get_id_name(cluster, "#cluster_name")
 
 
-def aggregate_get_cluster_names() -> base.dict_keys | None:
+def aggregate_get_cluster_names() -> Optional[base.dict_keys]:
     return aggregate_get_id_names("#cluster_name")
 
 
-def get_cluster_by_cluster_name(cluster_name: str) -> dict | None:
+def get_cluster_by_cluster_name(cluster_name: str) -> Optional[dict]:
     return aggregate_get_gem_by_id("#cluster_name", cluster_name)
