445de666365b6e5de3a1e7961f8276135babe7b5
Bill La Forge <laforge49@gmail.com>
2024-12-23 13:21:21 -0500

    Update gems_query.py

--- gems/facets/gems_query.py
+++ gems/facets/gems_query.py
@@ -1,10 +1,15 @@
-def get_gf(gem: dict | None) -> list | None:
+from typing import *
+
+from gems import base
+
+
+def get_gf(gem: Optional[base.Gem]) -> Optional[list]:
     if gem is None:
         return None
     return gem.get("GemsFacet")
 
 
-def get_gems(gem: dict, gem_parent: dict | None):
+def get_gems(gem: Optional[base.Gem], gem_parent: Optional[base.Gem]):
     yield gem, gem_parent
     gf = get_gf(gem)
     if gf is not None:
