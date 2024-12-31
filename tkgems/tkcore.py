971cc08da847fe58e4602045a4d41ca2543de682
Bill La Forge <laforge49@gmail.com>
2024-12-31 14:28:58 -0500

    command resource function

--- tkgems/tkcore.py
+++ tkgems/tkcore.py
@@ -110,7 +110,10 @@ def do_tkoptions(tkgem: base.Gem) -> dict:
     listvar_object = get_tkobject(tkgem, "ListVariableGemName")
     if listvar_object is not None:
         options["listvariable"] = listvar_object
-    # todo variable options
+    command_name = global_tags_query.get_command(tkgem)
+    if command_name is not None:
+        func = core.get_resource_function(command_name)
+        options["command"] = lambda: func(tkgem)
     return options
 
 
