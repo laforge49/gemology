5f59234f73e0b1cdf81f3b8456d811dc703a5e4a
Bill La Forge <laforge49@gmail.com>
2025-01-07 10:52:54 -0500

    Update global_tags_test.py

--- gems/facets/global_tags_test.py
+++ gems/facets/global_tags_test.py
@@ -1,7 +1,7 @@
 import pathlib
 
 from gems import core, base
-from gems.facets import global_tags_query
+from gems.facets import global_tags_query, global_tags_update
 from pdml import saver
 
 
@@ -11,7 +11,8 @@ def test(home_path: pathlib.Path) -> None:
     test_data_path = home_path / pathlib.Path("test data")
     print("test data path:", test_data_path)
     core.initialize(home_path)
-    global_tags_test = core.load(test_data_path / pathlib.Path("global_tags_test.pdml"))
+    global_tags_test_cluster = core.load(test_data_path / pathlib.Path("global_tags_test.pdml"))
+    global_tags_update.set_tag(global_tags_test_cluster, "cGemName:", ".foofoo")
     print()
     print("aggregate tag names:")
     print(global_tags_query.aggregate_get_tag_names())
@@ -20,6 +21,12 @@ def test(home_path: pathlib.Path) -> None:
     print("simpleGlobalTag:", global_tags_query.aggregate_get_tag_values("simpleGlobalTag"))
     print("aGemName:", global_tags_query.aggregate_get_tag_values("aGemName"))
     print("bGemName:", global_tags_query.aggregate_get_tag_values("bGemName"))
+    print("cGemName:", global_tags_query.aggregate_get_tag_values("cGemName"))
+    print()
+    print("global_tags_test")
+    saver.debug(global_tags_test_cluster)
+    print()
+    print("foofoo")
     saver.debug(global_tags_query.aggregate_get_gems_by_tag("bGemName", "Aggregate.unknown"))
 
 
