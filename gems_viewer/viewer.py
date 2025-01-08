c392bb7404b73d2a363208a2ee04dd1d645fbefc
Bill La Forge <laforge49@gmail.com>
2025-01-08 08:55:35 -0500

    Update viewer.py

--- gems_viewer/viewer.py
+++ gems_viewer/viewer.py
@@ -55,6 +55,10 @@ def init_listbox_gem(listbox_gem_gem: base.Gem):
     cluster_gem = global_ids_query.get_cluster_by_cluster_name(selected_cluster_name)
     load_gems(cluster_gem, listbox_gem_object, "")
     gem_index = base.findin(selected_gem_base_names, selected_gem_base_name)
+    if gem_index is None:
+        gem_index = 0
+    selected_listbox_gem_index = gem_index
+    listbox_gem_object.select_set(selected_listbox_gem_index)
     print(124, "todo")
 
 
